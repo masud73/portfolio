c94b4d15d3c3e5859f92f74fc74f72f97c4f71f0
Mohammed Abubakar <84784319+masud73@users.noreply.github.com>
2023-09-13 12:57:01 +0100

    Update social.jsx

--- src/components/social.jsx
+++ src/components/social.jsx
@@ -13,10 +13,10 @@ export default function Social()
                 <div className='flex-center' title='Github - masud73'>
                     <a href='https://www.github.com/masud73' rel='noopener noreferrer' className='flex-start-center' target='_blank'><Github id='github'/></a>
                 </div>
-                <div className='flex-center' title='Twitter - @____masud'>
+                <div className='flex-center' title='Twitter - @devmasud'>
                     <a href='https://www.twitter.com/____masud' rel='noopener noreferrer' className='flex-start-center' target='_blank'><Twitter id='twitter'/></a>
                 </div>
-                <div className='flex-center' title='Email at: mohdabbakarr@gmail.com'>
+                <div className='flex-center' title='Email at: masud727333@gmail.com'>
                     <a href='mailto:mohdabbakarr@gmail.com' rel='noopener noreferrer' className='flex-start-center'><Envelope id='email'/></a>
                 </div>
                 <div className='flex-center' title='Call +234 706 565 6558'>
