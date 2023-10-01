dacfa1f192e984775252aae3d07b0bf8a3356785
Mohammed Abubakar <84784319+masud73@users.noreply.github.com>
2023-10-01 18:06:21 +0100

    Update social.jsx

--- src/components/social.jsx
+++ src/components/social.jsx
@@ -7,17 +7,17 @@ export default function Social()
     return (
        <>
             <header>
-                <span className='title'>Connect with me</span>
+                <span className='title'>CONNECT WITH ME</span>
             </header>
             <div className='social-icons-container flex-start-center'>
                 <div className='flex-center' title='Github - masud73'>
-                    <a href='https://www.github.com/masud73' rel='noopener noreferrer' className='flex-start-center' target='_blank'><Github id='github'/></a>
+                    <a href='https://www.github.com/masud67577573' rel='noopener noreferrer' className='flex-start-center' target='_blank'><Github id='github'/></a>
                 </div>
                 <div className='flex-center' title='Twitter - @devmasud'>
-                    <a href='https://www.twitter.com/____masud' rel='noopener noreferrer' className='flex-start-center' target='_blank'><Twitter id='twitter'/></a>
+                    <a href='https://www.twitter.com/DevMasud' rel='noopener noreferrer' className='flex-start-center' target='_blank'><Twitter id='twitter'/></a>
                 </div>
                 <div className='flex-center' title='Email at: masud727333@gmail.com'>
-                    <a href='mailto:mohdabbakarr@gmail.com' rel='noopener noreferrer' className='flex-start-center'><Envelope id='email'/></a>
+                    <a href='mailto:masud727333@gmail.com' rel='noopener noreferrer' className='flex-start-center'><Envelope id='email'/></a>
                 </div>
                 <div className='flex-center' title='Call +234 706 565 6558'>
                     <a href='tel:+2347065656558' rel='noopener noreferrer' className='flex-start-center'><Telephone id='phone'/></a>
