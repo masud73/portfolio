193bd54f68a121a65a23b870b57fee441a86354a
Mohammed Abubakar <84784319+masud73@users.noreply.github.com>
2023-10-01 17:49:19 +0100

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,17 +10,7 @@ export function AboutMe()
             <Card>
                 <Card.Body>
                     <Card.Text as='p'>
-                        Hi there, My name is Masud. I hail from Nigeria. I'm a full-stack web developer
-                        and I love creating stuffs for the web and command-line. <br /> <br />
-                        
-                        I can deliver the following services: <br />
-                    </Card.Text>
-                    <ul>
-                        <li>Static web pages</li>
-                        <li>Dynamic web apps</li>
-                        <li>Command-line programs</li>
-                        <li>Maintaining any CRUD web app</li>
-                    </ul>
+                        Greetings, I am Masud, a front-end web developer based in Nigeria, boasting 4 years of professional experience. My expertise lies in crafting web solutions with a strong commitment to excellence.
                 </Card.Body>
             </Card>
         </div>
